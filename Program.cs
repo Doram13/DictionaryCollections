bb09758630ad52e540d8c687c43d4a5c43eb8618
Doram13 <1991doram@gmail.com>
2019-03-21 00:01:03 +0100

    Dictionary Collections

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace DictionaryCollections
 {
@@ -10,8 +11,26 @@ namespace DictionaryCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Hashtable myHashTable = new Hashtable();
+            myHashTable.Add('1', "one");
+            myHashTable.Add('2', "two");
+            myHashTable.Add('3', "three");
+            myHashTable.Add('4', "four");
+            myHashTable.Add('5', "five");
+            myHashTable.Add('6', "six");
+            myHashTable.Add('7', "seven");
+            myHashTable.Add('8', "eight");
+            myHashTable.Add('9', "nine");
+            string numbers = "982137465";
+            foreach (char number in numbers)
+            {
+                if (Char.IsDigit(number))
+                {
+                    Console.WriteLine(myHashTable[number]);
+                }
+            }
+            Console.Read();
+
         }
     }
 }
